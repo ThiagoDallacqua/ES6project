c522f48527f06f48578144bbd9f15401945719b5
Thiago Dallacqua <thiagodallacqua@outlook.com>
2017-05-10 14:53:38 -0300

    added a commentarie of how to create the same AJAX call but with the fetch API

--- client/js/app/services/HttpService.js
+++ client/js/app/services/HttpService.js
@@ -22,3 +22,18 @@ class HttpService{
     });
   }
 }
+
+/*
+com fetch fica assim:
+
+_handleErrors(res) {
+  if(!res.ok) throw new Error(res.statusText);
+  return res;
+}
+
+get(url) {
+  return fetch(url)
+          .then(res => this._handleErrors(res))
+          .then(res => res.json());
+}
+*/
